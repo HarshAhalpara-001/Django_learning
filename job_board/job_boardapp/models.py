8546f722d0d395825095be5df46baad0041d751e
HarshAhalpara-001 <ahalparaharsh1234@gmail.com>
2024-07-31 23:55:02 +0530

    Database connection

---
+++ job_board/job_boardapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class JobPosting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company = models.CharField(max_length=100)
+    salary = models.IntegerField()
+# makemigrations will add data but not commit
+# migrate for applies the data in the database
