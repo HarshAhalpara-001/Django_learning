c2589c97f6de77b9c4dd953c1620fceb565257eb
HarshAhalpara-001 <ahalparaharsh1234@gmail.com>
2024-08-03 01:45:03 +0530

    Job commit

--- job_board/job_boardapp/models.py
+++ job_board/job_boardapp/models.py
@@ -6,5 +6,6 @@ class JobPosting(models.Model):
     description = models.TextField()
     company = models.CharField(max_length=100)
     salary = models.IntegerField()
+    is_active = models.BooleanField(default=False)
 # makemigrations will add data but not commit
 # migrate for applies the data in the database
--- job_board/job_boardapp/views.py
+++ job_board/job_boardapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import JobPosting
 # Create your views here.
 def home(request):
-    return HttpResponse("Home page")
+    active = JobPosting.objects.filter(is_active=True)
+    context={
+        'job' : active
+    }
+    return render(request, 'job_boardapp/index.html',context)
